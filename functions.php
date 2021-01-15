93124de33479821f7642766f529838cf1598b277
joannamfolk <jfolk2@mail.greenriver.edu>
2021-01-15 15:32:21 -0500

    Separated function into functions.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    // A function to print an array, with $numbers as the parameter
+    function printArr($numbers) {
+        // A foreach loop representing each number in the array on a separate line
+        foreach ($numbers as $number) {
+            echo "$number <br>";
+        }
+    }
--- index.php
+++ index.php
@@ -34,14 +34,6 @@
     // An array holding a random assortment of numbers
     $numbers = array('7','9','8','9','8','8','6');
 
-    // A function to print an array, with $numbers as the parameter
-    function printArr($numbers) {
-        // A foreach loop representing each number in the array on a separate line
-        foreach ($numbers as $number) {
-            echo "$number <br>";
-        }
-    }
-
     // A function call
     printArr($numbers);
 
