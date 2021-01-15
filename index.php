5f4df37f1e45514c7654c558f4de34290af7ad42
joannamfolk <jfolk2@mail.greenriver.edu>
2021-01-15 15:21:39 -0500

    Created Index w/ Two PHP Blocks

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Name: Joanna Folk
+    Date: 01/15/2020
+    Files: index.php, ???
+    Purpose: To test the boundaries of git-commits -->
+
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Welcome to Jo's World </h1>
+
+</body>
+</html>
+
+<?php
+
+// Block for error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+?>
+
+<?php
+
+// Block for PHP Array Practice
+
+?>
